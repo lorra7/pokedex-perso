bc252b451f4c3b4dbd1735752e8f361500f47ffc
laura baratier <laura.baratierpro@gmail.com>
2024-04-07 18:43:19 +0200

    display pokemon img done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,24 +1,3 @@
-function PokemonCard() {
-  const pokemonTrainer = "Laura";
-  const pokemon = pokemonList[2];
-
-  return (
-    <div>
-      <h3>
-        Hello <strong>{pokemonTrainer}</strong> trainer !
-      </h3>
-      <figure>
-        {pokemon.imgSrc ? (
-          <img className="imgPokemon" src={pokemon.imgSrc} alt={pokemon.name} />
-        ) : (
-          <p>???</p>
-        )}
-
-        <figcaption>{pokemon.name}</figcaption>
-      </figure>
-    </div>
-  );
-}
 
 //Ajout de données : listes pokémon
 const pokemonList = [
@@ -37,5 +16,28 @@ const pokemonList = [
       "https://www.pokepedia.fr/images/thumb/1/17/Dracaufeu-RFVF.png/800px-Dracaufeu-RFVF.png",
   },
 ];
+
+
+function PokemonCard() {
+  const pokemonTrainer = "Laura";
+  const pokemon = pokemonList[2];
+
+  return (
+    <>
+      <h3>
+        Hello <strong>{pokemonTrainer}</strong> trainer !
+      </h3>
+      <figure>
+        {pokemon.imgSrc != null ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </>
+  );
+}
 
 export default PokemonCard;
